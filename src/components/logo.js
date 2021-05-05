a79a708790b0206bb4ea5d657919ec8731e4a67b
Priya Aswani <priya.aswani28139@gmail.com>
2021-05-05 14:22:41 +0400

    Create out the Logo component

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,9 +1,14 @@
 /** @jsx jsx */
-import { jsx, Image } from 'theme-ui';
-import { Link } from 'components/link';
+import { jsx, Image } from "theme-ui";
+import { Link } from "components/link";
 
 export default function Logo({ src, ...rest }) {
   return (
-    <h1>Logo</h1>
+    <Link
+      path="/"
+      sx={{ variant: "links.logo", display: "flex", mr: 15, cursor: "pointer" }}
+    >
+      <Image src={src} alt="logo" />
+    </Link>
   );
 }
